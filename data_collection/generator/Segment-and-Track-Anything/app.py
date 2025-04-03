bfcd76057f79eabf0ab4dca6d24f84f81d84eaa4
HilbertXu <hilbertxu@outlook.com>
2025-04-03 17:44:08 +0100

    remove the need of SAM example

--- data_collection/generator/Segment-and-Track-Anything/app.py
+++ data_collection/generator/Segment-and-Track-Anything/app.py
@@ -1095,7 +1095,7 @@ def seg_track_app():
             gr.Examples(
                 examples=[
                     # os.path.join(os.path.dirname(__file__), "assets", "840_iSXIa0hE8Ek.mp4"),
-                    os.path.join(os.path.dirname(__file__), "assets", "blackswan.mp4"),
+                    # os.path.join(os.path.dirname(__file__), "assets", "blackswan.mp4"),
                     # os.path.join(os.path.dirname(__file__), "assets", "bear.mp4"),
                     # os.path.join(os.path.dirname(__file__), "assets", "camel.mp4"),
                     # os.path.join(os.path.dirname(__file__), "assets", "skate-park.mp4"),
